63b89c332a574bb5b77b1427e591794a9a2fe62d
Nithin Kodithala <121954855+nithinkodithala@users.noreply.github.com>
2025-11-21 22:47:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def convert():
         return jsonify({"toon_output": f"Error: {e}"})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5010)
+    app.run(host="0.0.0.0",debug=True, port=5010)
